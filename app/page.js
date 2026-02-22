58737539195f75f07283dd203157ce43c804b1ff
Savit Pawar <137122831+Ironboss456@users.noreply.github.com>
2026-02-22 14:53:25 -0800

    created the our services section of the home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import ServicesHomePage from "@/components/ServicesHomePage";
 
 // home to label page; to be changed
 export default function Home() {
   return (
     <div>
         <Navbar/>
+        <ServicesHomePage/>
         home
     </div>
   );
