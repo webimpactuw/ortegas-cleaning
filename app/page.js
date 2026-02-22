cb1f217bcfc321c0c7ed56410407933ba7d3da0a
DavidOh1606 <david.oh1606@gmail.com>
2026-02-21 18:45:38 -0800

    set up background linear gradient

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Hero from "@/components/Hero";
 // home to label page; to be changed
 export default function Home() {
   return (
-    <div>
+    <div className="bg-linear-to-b from-[#EFF6FF] from-10% via-[#FFF9E0] via-40% to-[#FFFFFF] h-[1750px] to-70%">
         <Navbar/>
         home
         <Hero/>
