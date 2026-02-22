e8743e5c9c55400f268a54618be180f6536133e0
Yonie <164077864+Isaiahriveraa@users.noreply.github.com>
2026-02-22 14:06:04 -0800

    feat: add Footer to global layout and pin it to bottom of page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "../components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={"antialiased flex flex-col min-h-screen"}>
+        <div className="flex-grow">{children}</div>
+        <Footer />
       </body>
     </html>
   );
