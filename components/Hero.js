db13b51aa9219463cb6a49a2af9b5704a944366d
DavidOh1606 <david.oh1606@gmail.com>
2026-02-27 17:27:14 -0800

    Change font and font size

--- components/Hero.js
+++ components/Hero.js
@@ -2,6 +2,9 @@ import Link from "next/link";
 import Image from "next/image";
 import hero_icon from "../public/hero_icon.png";
 import Bubble from "./Bubble";
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Hero() {
     return (
@@ -15,13 +18,13 @@ export default function Hero() {
 
             <Image src={hero_icon} alt="ortega's cleaning logo" className="z-1"></Image>
 
-            <div className="text-[#4A4A4ACC] w-[630px] text-[20px] text-center">
+            <div className="text-[#4A4A4ACC] w-[630px] text-[1.2rem] text-center font-[inter]">
                 Delivering detailed, reliable residential and commercial 
                 cleaning services across King County
             </div>
 
             <Link className="px-8 py-4 bg-linear-to-r from-[#2C3E50] 
-                            to-[#4A90E2] rounded-[16px] text-white" 
+                            to-[#4A90E2] rounded-[16px] text-[0.9rem] text-white font-[inter]" 
                 href="/contact">Get Your Free Quote</Link>
 
         </div>
