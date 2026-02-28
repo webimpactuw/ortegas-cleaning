77113bfad30e5ff176047feb85be2f25fc6458f8
DavidOh1606 <david.oh1606@gmail.com>
2026-02-27 17:11:22 -0800

    Clean up merge

--- app/page.js
+++ app/page.js
@@ -6,11 +6,12 @@ import ServicesHomePage from "@/components/ServicesHomePage";
 // home to label page; to be changed
 export default function Home() {
   return (
-    <div className="bg-linear-to-b from-[#EFF6FF] from-10% via-[#FFF9E0] via-50% to-[#FFFFFF] h-[1300px] to-70%">
+    <div className="bg-linear-to-b from-[#EFF6FF] from-10% via-[#FFF9E0] via-50% to-[#FFFFFF] to-70%">
         <Navbar/>
+        <Hero/>
         <ServicesHomePage/>
         home
-        <Hero/>
+
     </div>
   );
 }
--- components/Hero.js
+++ components/Hero.js
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
 import hero_icon from "../public/hero_icon.png";
-import bubble from "../public/bubble.png";
 
 export default function Hero() {
     return (
         <div className="flex flex-col items-center gap-[16px]">
-
+            
             <Image src={hero_icon} alt="ortega's cleaning logo" className="z-1"></Image>
 
             <div className="text-[#4A4A4ACC] w-[630px] text-[20px] text-center">
@@ -18,8 +17,6 @@ export default function Hero() {
                             to-[#4A90E2] rounded-[16px] text-white" 
                 href="/contact">Get Your Free Quote</Link>
 
-            <Image src={bubble} alt="bubble" className="absolute top-40 right-80"></Image>
-            {/* <div className="size-18 rounded-full bg-radial from-[#C4C4C400] to-[#A8D5E280]"></div> */}
         </div>
     );
 }
