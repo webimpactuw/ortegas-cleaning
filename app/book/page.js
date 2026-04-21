db86b440c3efb277e7fd10b35fce5d2050bcf383
Caden Saltzberg <solavolantes@gmail.com>
2026-04-20 20:38:31 -0700

    modified booking page

--- app/book/page.js
+++ app/book/page.js
@@ -1,6 +1,6 @@
-import BookingForm from "@/components/BookingForm";
+import BookingForm from "@/components/booking/BookingForm";
 import Navbar from "@/components/Navbar";
-import TermsConditions from "@/components/TermsConditions";
+import TermsConditions from "@/components/booking/TermsConditions";
 
 
 // book to label page; to be changed
@@ -10,9 +10,9 @@ export default function Book() {
             <Navbar/>
             <div className="max-w-6xl mx-auto px-6 py-12">
 
-                <h1 className="text-3xl font-semibold text-center mb-10">
+                <h3 className="font-[Playfair_Display] font-semibold text-4xl leading-[40px] tracking-normal text-center text-[#2E4E65] mb-4">
                     Book a Service
-                </h1>
+                </h3>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
                     <div className="flex">
